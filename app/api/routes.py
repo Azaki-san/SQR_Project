ee7f4ff4ac015ec4ce75fb4237e4019cbd6f6667
Nikolai Kuzmin <englia228@gmail.com>
2025-05-08 06:31:17 +0300

    route ping

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, UploadFile, Request
 from app.services.sync import get_video_filename_path
 from fastapi.responses import FileResponse
 
+from app.utils.viewer_count import viewer_ping, get_viewer_count
+
 router = APIRouter()
 
 
@@ -9,3 +11,9 @@ router = APIRouter()
 def get_video_file():
     path, filename = get_video_filename_path()
     return FileResponse(path, media_type="video/mp4", filename=filename)
+
+@router.post("/ping")
+def ping(request: Request):
+    viewer_id = request.client.host
+    viewer_ping(viewer_id)
+    return {"viewers": get_viewer_count()}
